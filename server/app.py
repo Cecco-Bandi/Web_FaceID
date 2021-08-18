1d675c691d5e180b0a256d589a942ad929669bb5
Francesco Carrabino <francesco.carrabino@gmail.com>
2021-08-19 00:35:47 +0200

    added face_recognition

--- server/app.py
+++ server/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import face_recognition
+
+image = face_recognition.load_image_file("biden.jpeg")
+face_landmarks_list = face_recognition.face_landmarks(image)
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return("Hello")
+    return(str(face_landmarks_list))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
